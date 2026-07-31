55ae751a43306b67dcc78a2b4bb1e4f94782d51a
pablobidiaki <pablobidiaki@hotmail.com>
2026-07-31 17:07:56 -0300

    chore: deleteing icons import

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,6 @@
 import Hero from "@/components/Hero/Hero";
 import texts from "@/constants/texts";
 
-import { Plane, NotebookPen, BrainCircuit, FileUser, Clock, ScrollText, MapPin, CalendarDays, DollarSign } from "lucide-react";
-
-
 export default function Home() {
   return (
     <div>
