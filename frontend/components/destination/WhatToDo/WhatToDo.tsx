20d31db9ca5e09d8c324930b77f8d69420feff79
pablobidiaki <pablobidiaki@hotmail.com>
2026-08-25 15:42:44 -0300

    feat: create WhatToDo components in destination[id] screen

--- frontend/components/destination/WhatToDo/WhatToDo.tsx
+++ frontend/components/destination/WhatToDo/WhatToDo.tsx
@@ -1,25 +1,81 @@
+"use client"
+
+import { useState } from "react";
 import texts from "@/constants/texts";
 import WhatToDoCard from "./WhatToDoCard";
 import { Destination } from "@/interfaces/destination.interface";
-import { ShieldQuestionMark } from "lucide-react";
+import { ChefHat, Drama, Leaf, PartyPopper, ShieldQuestionMark, Store } from "lucide-react";
 
 interface DestinationBodyProps {
     destination: Destination[]
 }
 
-export default function WhatToDo({ destination }: DestinationBodyProps){
-    return(
+export default function WhatToDo({ destination }: DestinationBodyProps) {
+    const [selectedCard, setSelectedCard] = useState<string | null>(null);
+    return (
         <div className="mx-4 mt-10">
             <div className="flex items-center gap-2 mb-5">
                 <ShieldQuestionMark size={35} className="text-primary-color font-medium" />
                 <h1 className="text-3xl text-primary-color font-medium">{texts.destination.what_to_do}</h1>
             </div>
             <div className=" grid grid-cols-5 gap-10">
-                <WhatToDoCard title={texts.destination.nature} texts={destination[0].whatToDo.nature}/>
-                <WhatToDoCard title={texts.destination.culture} texts={destination[0].whatToDo.culture}/>
-                <WhatToDoCard title={texts.destination.tipicalFoods} texts={destination[0].whatToDo.tipicalFood}/>
-                <WhatToDoCard title={texts.destination.famousStores} texts={destination[0].whatToDo.famousStores}/>
-                <WhatToDoCard title={texts.destination.nightLife} texts={destination[0].whatToDo.nightLife}/>
+                <WhatToDoCard icon={<Leaf size={40}/>}
+                    title={texts.destination.nature}
+                    texts={destination[0].whatToDo.nature}
+                    isOpen={selectedCard === texts.destination.nature}
+                    onClick={() =>
+                        setSelectedCard(
+                            selectedCard === texts.destination.nature
+                                ? null
+                                : texts.destination.nature
+                        )}
+                />
+
+                <WhatToDoCard icon={<Drama size={40}/>}
+                    title={texts.destination.culture}
+                    texts={destination[0].whatToDo.culture}
+                    isOpen={selectedCard === texts.destination.culture}
+                    onClick={() =>
+                        setSelectedCard(
+                            selectedCard === texts.destination.culture
+                                ? null
+                                : texts.destination.culture
+                        )}
+                />
+
+                <WhatToDoCard icon={<ChefHat size={40} />}
+                    title={texts.destination.tipicalFoods}
+                    texts={destination[0].whatToDo.tipicalFood}
+                    isOpen={selectedCard === texts.destination.tipicalFoods}
+                    onClick={() =>
+                        setSelectedCard(
+                            selectedCard === texts.destination.tipicalFoods
+                                ? null
+                                : texts.destination.tipicalFoods
+                        )}
+                />
+                <WhatToDoCard icon={<Store size={40}/>}
+                    title={texts.destination.famousStores}
+                    texts={destination[0].whatToDo.famousStores}
+                    isOpen={selectedCard === texts.destination.famousStores}
+                    onClick={() =>
+                        setSelectedCard(
+                            selectedCard === texts.destination.famousStores
+                                ? null
+                                : texts.destination.famousStores
+                        )}
+                />
+                <WhatToDoCard icon={<PartyPopper size={40}/>}
+                    title={texts.destination.nightLife}
+                    texts={destination[0].whatToDo.nightLife}
+                    isOpen={selectedCard === texts.destination.nightLife}
+                    onClick={() =>
+                        setSelectedCard(
+                            selectedCard === texts.destination.nightLife
+                                ? null
+                                : texts.destination.nightLife
+                        )}
+                />
             </div>
         </div>
     )
--- frontend/components/destination/WhatToDo/WhatToDoCard.tsx
+++ frontend/components/destination/WhatToDo/WhatToDoCard.tsx
@@ -1,15 +1,31 @@
-interface WhatToDoCardProps{
+import { ReactNode } from "react"
+
+interface WhatToDoCardProps {
+    icon: ReactNode
     title: string
     texts: string[]
+    isOpen: boolean
+    onClick: () => void
 }
 
-export default function WhatToDoCard({title, texts}: WhatToDoCardProps){
-    return(
-        <div className="bg-white rounded-2xl border border-gray-200 p-2">
-            <h1 className="text-primary-color font-medium text-xl mb-2">{title}</h1>
-            {texts.map((text) => (
-                <p className="text-second-color font-light text-sm">- {text}</p>
-            ))}
+export default function WhatToDoCard({ icon, title, texts, isOpen, onClick }: WhatToDoCardProps) {
+    return (
+        <div className="relative  transition-all duration-300 hover:scale-105">
+            <div onClick={onClick}
+                 className="bg-white rounded-2xl border border-gray-200 py-10 cursor-pointer">
+                <div className="flex flex-col items-center">
+                    <span>{icon}</span>
+                    <h1 className="text-primary-color font-medium text-xl mb-2 text-center">{title}</h1>
+                </div>
+            </div>
+
+            {isOpen && (
+                <div className="absolute left-0 top-full z-20 w-full mt-2 bg-white border border-gray-200 rounded-2xl p-4 shadow-lg">
+                    {texts.map((text, index) => (
+                        <p key={index} className="text-second-color font-light text-sm">- {text} </p>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
